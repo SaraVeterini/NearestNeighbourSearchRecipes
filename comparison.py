da777d4493daf945365c9c3e095d880771151841
r3kall <lnz.rutigliano@gmail.com>
2016-12-08 14:41:00 +0100

    compare features

--- comparison.py
+++ comparison.py
@@ -60,14 +60,26 @@ def compare(lsh_iterations):
     jaccard_list = Jaccard(shingles_map).jaccardNeighbours
 
     # compute minhashing and lsh.
+    lsh_length = []
     intersections = []
+    false_positives = []
     for i in range(lsh_iterations):
         signatures = DocMinHashSignatures(shingles_map, saveFile=False).minHashDocuments
         lsh_list = Lsh(signatures).lshNeighbours
         intersection = intersect_lists(jaccard_list, lsh_list)
+        lsh_length.append(len(lsh_list))
         intersections.append(len(intersection))
+        false_positives.append(len(lsh_list) - len(intersection))
 
-    print sum(intersections) / lsh_iterations
+    ml = (float(sum(lsh_length)) / float(lsh_iterations))
+    fp = (float(sum(false_positives)) / float(lsh_iterations))
+    ma = (float(sum(intersections)) / float(lsh_iterations))
+
+    print '\nLSH Mean false-positives: %f' % (fp / ml)
+
+    print '\nLSH Mean accuracy in LSH: %f' % (ma / ml)
+
+    print '\nEnd'
 
 
 if __name__ == '__main__':
